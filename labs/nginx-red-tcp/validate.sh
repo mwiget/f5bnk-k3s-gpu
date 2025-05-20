7d7712a854ccb1dda5128ad8f8f6798c2b444803
Marcel Wiget <m.wiget@f5.com>
2025-05-20 03:55:06 +0000

    adding test/512kb

--- labs/nginx-red-tcp/validate.sh
+++ labs/nginx-red-tcp/validate.sh
@@ -5,6 +5,15 @@ set -e
 client=lake1    # host with route to external BNK interface p0
 
 echo ""
+set -x
+kubectl get f5-bnkgateways
+kubectl get gatewayclass f5-gateway-class
+kubectl get gateway -n red f5-l4-gateway
+kubectl get l4route -n red
+set +x
+echo ""
+
+echo ""
 echo -n "extract assigned IP address from f5-l4-gateway ... "
 ip=$(kubectl get gateway -n red f5-l4-gateway -o json | jq -r '.status.addresses[] | select(.type == "IPAddress") | .value')
 echo $ip
@@ -19,15 +28,10 @@ until ssh $client ping -c3 $ip; do
 done
 
 echo ""
-set -x
-kubectl get f5-bnkgateways
-kubectl get gatewayclass f5-gateway-class
-kubectl get gateway -n red f5-l4-gateway
-kubectl get l4route -n red
-set +x
-echo ""
-
-echo ""
 echo "Test with curl from client $client ..."
 echo ""
 ssh $client curl -Is http://$ip
+
+echo ""
+echo "Downloading 512kb payload from $ip ..."
+ssh $client "curl -s -w \"\nTime: %{time_total}s\nSpeed: %{speed_download} bytes/s\n\" -o /dev/null http://198.19.19.50/test/512kb"
