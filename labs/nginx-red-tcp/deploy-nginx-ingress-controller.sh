6b65d6995b88e89b67c3f43b50c9a1978470a784
Marcel Wiget <m.wiget@f5.com>
2025-06-01 14:27:39 +0000

    reshuffle

--- labs/nginx-red-tcp/deploy-nginx-ingress-controller.sh
+++ labs/nginx-red-tcp/deploy-nginx-ingress-controller.sh
@@ -9,4 +9,9 @@ helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
   --set controller.service.nodePorts.http=30088 \
   --set controller.service.nodePorts.https=30443
 
+until kubectl rollout status deployment/ingress-nginx-controller --timeout=5s; do
+  echo "Waiting for ingress-nginx-controller to be ready..."
+  sleep 5
+done
+
 kubectl apply -f ingress-resource.yaml 
--- labs/nginx-red-tcp/deploy.sh
+++ labs/nginx-red-tcp/deploy.sh
@@ -10,6 +10,7 @@ kubectl get ns red || kubectl create ns red
 
 kubectl apply -f nginx-red-deployment.yaml
 kubectl apply -f nginx-red-tcp-gw-api.yaml
+
 until kubectl wait --for=condition=Ready pods --all -n red; do
   echo "trying again in 5 secs ..."
   echo "sleep 5"
