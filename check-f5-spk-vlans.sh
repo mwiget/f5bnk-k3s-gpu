d85dc77cf4a3728768261323db12dec741f557a5
Marcel Wiget <m.wiget@f5.com>
2025-06-02 16:58:12 +0000

    default to large on dpu

--- check-f5-spk-vlans.sh
+++ check-f5-spk-vlans.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+kubectl get f5-spk-vlans
 until [ "$(kubectl get f5-spk-vlans | tail -n +2 | grep -cv 'CR config sent to all grpc endpoints')" -eq 0 ]; do
   echo "waiting for all f5-spk-vlans configured ..."
   kubectl get f5-spk-vlans
