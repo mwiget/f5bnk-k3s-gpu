b41a9699316cfcd6c17889e4aeca08c8a6754f35
Marcel Wiget <m.wiget@f5.com>
2025-06-03 09:41:01 +0000

    wait for node to show up before tainting it

--- add-dpu1-node.sh
+++ add-dpu1-node.sh
@@ -4,6 +4,8 @@ set -e
 SECONDS=0
 
 DPU_IP="192.168.100.2"
+DPU_NAME="dpu1"
+
 KUBECONFIG_PATH="$HOME/.kube/config"
 K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 
@@ -18,10 +20,16 @@ ssh ubuntu@$DPU_IP "sudo mv k3s-resolv.conf /etc/"
 
 ssh ubuntu@$DPU_IP "curl -sfL https://get.k3s.io | K3S_URL=https://${MASTER_IP}:6443 K3S_TOKEN=${K3S_TOKEN} INSTALL_K3S_EXEC='--resolv-conf=/etc/k3s-resolv.conf' sh -"
 
-for node in $(kubectl get nodes | grep ' Ready '| awk '{print $1}' | grep 'dpu'); do
-  kubectl taint node $node dpu=true:NoSchedule || true
-  kubectl label nodes $node nvidia.com/gpu.deploy.operands=false
+echo ""
+echo "wait for dpu node $DPU_NAMEto show up ..."
+until kubectl get node $DPU_NAME &>/dev/null; do
+  echo "Waiting for $DPU_NAME to register..."
+  sleep 2
 done
+echo "Node $DPU_NAME is now registered."
+
+kubectl taint node $DPU_NAME dpu=true:NoSchedule
+kubectl label nodes $DPU_NAME nvidia.com/gpu.deploy.operands=false
 
 kubectl wait --for=condition=Ready pods --all -n kube-system --timeout 300s
 kubectl wait --for=condition=Ready pods --all -n calico-system --timeout 300s
